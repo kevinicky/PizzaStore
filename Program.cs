b74dda980898310a396ba07ec2610160a90c8543
Kevin Nicky Setiawan <kevinicky@proton.me>
2024-07-18 00:04:45 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas/{id}", (UInt16 id) => PizzaDb.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDb.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDb.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDb.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (UInt16 id) => PizzaDb.RemovePizza(id));
+
+
+app.Run();
